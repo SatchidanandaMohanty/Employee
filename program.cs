dcd92a948d3713e9dca36235b026688d5812100b
Satchidananda <satchidanandamohanty1@gmail.com>
2022-11-27 19:56:23 +0530

    new proj

---
+++ program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace employee_project
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+        }
+    }
+}
